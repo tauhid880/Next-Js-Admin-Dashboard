f70f9489de316a543f5a648572be8be5f22b5f2f
tauhid880 <tauhid880@gmail.com>
2023-11-19 14:36:15 +0600

    home, user & products page done

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,25 @@
+import styles from "../ui/dashboard/dashboard.module.css";
+import Card from "../ui/dashboard/card/card";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
+import Transactions from "../ui/dashboard/transactions/transactions";
+import Chart from "../ui/dashboard/chart/chart";
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.card}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
